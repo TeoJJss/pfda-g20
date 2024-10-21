ae23423ff002b76d2143a43e1b9b7719cb7f86e7
LeongHC <hueychian.LHC@gmail.com>
2024-10-21 22:26:35 +0800

    Update main.R

--- main.R
+++ main.R
@@ -29,3 +29,87 @@ credit_risk_df = credit_risk_df[, selected_cols] # select required
 head(credit_risk_df)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##standardise
+
+#check variable type
+str(credit_risk_df)
+
+#for char variable
+
+char_cols = c(
+  "purpose", "employment", "property_magnitude", "job", 
+  "credit_history")
+credit_risk_df[char_cols]<-lapply(credit_risk_df[char_cols],tolower) #to turn the data to lowercase
+credit_risk_df[char_cols]<-lapply(credit_risk_df[char_cols],factor)
+
+
+
+#for numerical
+
+#age
+summary(credit_risk_df$age)
+has_decimal<-any(credit_risk_df$age %% 1!=0)
+
+invalid_age <- sum(credit_risk_df$age != floor(credit_risk_df$age))
+invalid_age
+
+if (has_decimal) {
+  print("There are decimal values in the age variable.")
+} else {
+  print("All values in the age variable are whole numbers.")
+}
+
+#installment_commitment
+summary(credit_risk_df$installment_commitment)
+summary(credit_risk_df$existing_credits)
+
+rounddata<-c("age","installment_commitment","existing_credits")
+credit_risk_df[rounddata]<-lapply(credit_risk_df[rounddata],function(x) round(x, 0))
+
+check_decimal<-any( credit_risk_df$age %% 1!=0)
+check_decimal
+check_decimal<-any( credit_risk_df$installment_commitment %% 1!=0)
+check_decimal
+check_decimal<-any( credit_risk_df$existing_credits %% 1!=0)
+check_decimal
+
+#check range
+age_range<-range(credit_risk_df$age,na.rm = TRUE)
+age_range
+
+ic_range<-range(credit_risk_df$installment_commitment,na.rm = TRUE)
+ic_range
+
+ec_range<-range(credit_risk_df$existing_credits,na.rm = TRUE)
+ec_range
+
+#check categories
+levels(credit_risk_df$credit_history)
+levels(credit_risk_df$credit_history)
+levels(credit_risk_df$credit_history)
+levels(credit_risk_df$credit_history)
+
